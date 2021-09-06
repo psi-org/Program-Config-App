81324c4a17d4ce40b3a4cbd731755588026ff44d
kimbalear <kimbalear@gmail.com>
2021-09-06 10:48:13 +0200

    update

---
+++ programConfig/scripts/components/sub_nav.js
@@ -0,0 +1,9 @@
+
+$('template-download').on({
+    "mouseover": function () {
+        this.src = 'images/i-download.svg';
+    },
+    "mouseout": function () {
+        this.src = 'images/tile_4.jpg';
+    }
+});
