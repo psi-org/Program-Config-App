5469f436fbb3c90f9bfb2855798b8051387b729f
Utsav Ashish Koju <utsavkoju@hotmail.com>
2021-12-07 11:45:44 -0330

    error counter icon placement fixed

--- src/components/STG_Details/Errors.js
+++ src/components/STG_Details/Errors.js
@@ -2,7 +2,7 @@ import error_svg from './../../images/i-error.svg';
 const Errors = (props) => {
     return  <>
                 <img src={error_svg} alt={"err"}/>
-                <div className={"ml_item-ce"}>
+                <div className={"ml_item-cw"}>
                     {props.counts}
                 </div>
             </>
