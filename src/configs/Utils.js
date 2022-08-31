02d853390c3403ef4a393177617b4a29d8fe0f13
Mario Lopez <00046317@uca.edu.sv>
2022-08-30 22:45:36 -0600

    New version check

--- src/configs/Utils.js
+++ src/configs/Utils.js
@@ -53,62 +53,8 @@ function characterPos(chr) {
   return chr.charCodeAt(0);
 }
 
-function versionCompare(v1, v2, options) {
-  var lexicographical = (options && options.lexicographical) || false,
-    zeroExtend = (options && options.zeroExtend) || true,
-    v1parts = (v1 || "0").split('.'),
-    v2parts = (v2 || "0").split('.');
-
-  function isValidPart(x) {
-    return (lexicographical ? /^\d+[A-Za-zαß]*$/ : /^\d+[A-Za-zαß]?$/).test(x);
-  }
-
-  if (!v1parts.every(isValidPart) || !v2parts.every(isValidPart)) {
-    return NaN;
-  }
-
-  if (zeroExtend) {
-    while (v1parts.length < v2parts.length) v1parts.push("0");
-    while (v2parts.length < v1parts.length) v2parts.push("0");
-  }
-
-  if (!lexicographical) {
-    v1parts = v1parts.map(function (x) {
-      var match = (/[A-Za-zαß]/).exec(x);
-      return Number(match ? x.replace(match[0], "." + x.charCodeAt(match.index)) : x);
-    });
-    v2parts = v2parts.map(function (x) {
-      var match = (/[A-Za-zαß]/).exec(x);
-      return Number(match ? x.replace(match[0], "." + x.charCodeAt(match.index)) : x);
-    });
-  }
-
-  for (var i = 0; i < v1parts.length; ++i) {
-    if (v2parts.length == i) {
-      return 1;
-    }
-
-    if (v1parts[i] == v2parts[i]) {
-      continue;
-    }
-    else if (v1parts[i] > v2parts[i]) {
-      return 1;
-    }
-    else {
-      return -1;
-    }
-  }
-
-  if (v1parts.length != v2parts.length) {
-    return -1;
-  }
-
-  return 0;
-}
-
 export function versionIsValid(serverVersion, limitMin, limitMax){
-  let coercedVersion = coerce(serverVersion)
-  //return versionCompare(serverVersion, limitMin) >= 0 && versionCompare(serverVersion, limitMax) <= 0;
+  let coercedVersion = coerce(serverVersion).version
   return gte(coercedVersion, coerce(limitMin)) && lte(coercedVersion, coerce(limitMax));
 }
 
