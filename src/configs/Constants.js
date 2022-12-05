a3603de3d581cff76f902ab0fd52781c53fa948d
Mario Lopez <00046317@uca.edu.sv>
2022-12-05 09:51:54 -0600

    New Metadata Package

--- src/configs/Constants.js
+++ src/configs/Constants.js
@@ -11,12 +11,12 @@ import DateIcon from '@mui/icons-material/CalendarToday';
 import TimeIcon from '@mui/icons-material/AccessTime';
 
 const BUILD_VERSION = "1.6.2"
-const BUILD_DATE = "December 14th, 2022"
+const BUILD_DATE = "December 5th, 2022"
 const MIN_VERSION = "2.36"
 const MAX_VERSION = "2.38.1.1"
 
 const PCA_METADATA_VERSION = "1.1.0"
-const H2_METADATA_VERSION = "1.1.1"
+const H2_METADATA_VERSION = "1.1.2"
 
 const PCA_ATTRIBUTES = ["yB5tFAAN7bI","haUflNqP85K"]
 const PCA_OPTION_SETS = ["TOcCuCN2CLm"]
