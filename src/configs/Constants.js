8478b94e238845dece961ec3c0aa1956dc8c5e5d
Marlpzg <milopezg@uca.edu.sv>
2025-01-17 15:57:05 -0600

    Updated version

--- src/configs/Constants.js
+++ src/configs/Constants.js
@@ -25,8 +25,8 @@ import TextIcon from '@mui/icons-material/TextFields';
 import TimelineIcon from '@mui/icons-material/Timeline';
 import React from 'react';
 
-const BUILD_VERSION = "2.1.2";
-const BUILD_DATE = "Dec 13, 2024";
+const BUILD_VERSION = "2.2.0";
+const BUILD_DATE = "Jan 25, 2025";
 const MIN_VERSION = "2.36.x";
 const MAX_VERSION = "2.41.x";
 
